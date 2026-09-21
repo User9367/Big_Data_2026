5a2c2cc82d8ffb1d08f60160124038f481c52e17
seva <seva.r2d2@gmail.com>
2026-09-21 19:42:50 +0300

    second task

--- 2026_09_14_sort_2_vars/simple_implementation/main.cpp
+++ 2026_09_14_sort_2_vars/simple_implementation/main.cpp
@@ -1,16 +1,40 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 
 void my_sort(int *arr, const int size);
 
-// TODO Интерфейс пользователя должен быть на русском языке
 int main() {
-    // TODO Пользователь вводит размер массив и элементы массива
-
-	// TODO вызвается void my_sort(int *arr, int size)
-	
-	// TODO Выводится первоначальный массив и отсортированный
+    int size_;
+    cout << "Введите размер массива: ";
+    cin >> size_;
+    cout << '\n';
+    vector<int> mas;
+    cout << "Вводите элементы массива: " << '\n';
+    for (int i=1; i<=size_; i+=1) {
+        int el;
+        cin >> el;
+        mas.push_back(el);
+    }
+    cout << "Изначальный массив: ";
+    for (int i=1; i<=size_; i+=1) {
+        cout << mas[i - 1] << ' ';
+    }
+    cout << '\n';
+    my_sort(mas.data(), size_);
+    return 0;
 }
 
 void my_sort(int *arr, const int size) {
-	// TODO
+    for (int i=0; i<size; i+=1) {
+        for (int j=0; j<size-1; j+=1) {
+            if (arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+    cout << "Отсортированный массив: ";
+    for (int i=0; i<size; i+=1) {
+        cout << arr[i] << ' ';
+    }
 }
